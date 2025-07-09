2a3ed8b1848eacda201ceda450ed09cca892c9bd
sdtredhuytrdvb <fyou84622@gmail.com>
2025-07-09 16:05:55 +0800

    Class 3

--- TesT.py
+++ TesT.py
@@ -11,6 +11,7 @@
 # d = -3.14159265358
 # print(floor(d))  # 類似無條件捨去(小數點後面的數字)
 # print(abs(d))
+
 # e = 4
 # f = 9
 # g = 16
@@ -24,7 +25,6 @@
 # print(max(d, e, f, g, h, j))  # 結果為數字,非變數
 # print(ceil(d))
 # str(d)
-# # print(d.index("5"))  # ???
 # print(round(d))  # 正負數相反
 # pi = 3.14
 # r = float(input("請輸入半徑"))
--- pages/class3-1.py
+++ pages/class3-1.py
@@ -10,3 +10,66 @@
 #        print("?")
 #    for i in range(?,?,?):
 #        print("?")
+# 25..copy
+# 功能:複製
+# 26.n = [1,2,3,"a","b","c"]
+# for i in range(0,len(n)):
+#     print(n[i])
+# for i in n:
+#     print(i)
+
+# 列表篇(list):
+# 1. []（不是函數的[] 功能不一樣
+#   功能：處理複雜資料（列表裡的括號
+#        （備註：字串的第一個數字是第0位-1是倒數第一位[?：?]
+#          問號一是從哪位開始取
+#          問號二是一路取到第幾位
+#          但不包含填的那一位
+#          問號一也可以不填
+#          代表從問號二開始取
+#          一直取到字串開頭
+#          Belike：[：?]
+#          問號二也可以不填
+#          代表從問號一的數開始取
+#          一直取到字串結束
+#          Belike：[？：]
+#          (情況三:
+#          [?1:?2:?3])
+#          ?1,?2上面已說明
+#          ?3是兩個連續的數差(特殊用法[::?])
+# 2..append()
+#   功能:在列表後面加入
+# 3..remove()
+#   功能:從列表中刪除
+# 4..pop()
+#   功能:從列表中刪除後取出
+# 5.sort()
+#   功能:列表排序
+
+# 補充知識:
+# # callby value
+# # a = 1
+# # b = a
+# # b = 2
+
+# # call by reference
+# # a = [1,2,3]
+# # b = a
+# # b[0] = 2
+# =
+# # a = [1,2,3]
+# # b = a.copy()
+# # b[0] = 2 #(代替方案)
+
+# f = open("pages\class1-1.py", "n", encoding="utf-8")
+# content = f.read()
+# print(content)
+# f.close()
+# with open("pages\class1-1.py", "n", encoding="utf-8") as f:
+#     content = f.read()
+#     print(content)
+
+# filename = "class1.md"
+# print(filename.endswith(".md"))
+# filename2 = "notes.txt"
+# print(filename2.endswith(".md"))
